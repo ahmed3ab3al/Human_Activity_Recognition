b31b2c7315abd6d5b9b6a80a48653477952d086d
Mohammed Ashraf <mohamedasharf55@gmail.com>
2024-03-31 14:59:07 +0200

    Edit AuthBloc

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,13 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project/features/auth/presentation/views/widget/login_view_body.dart';
+import '../../../../core/api/dio_helper.dart';
+import '../view_models/cubit.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return BlocProvider(
+        create: (context) => AppAuthCubit( DioHelper(dio: Dio()),),
+    child: Scaffold(
       body: LoginViewBody(),
-    );
+    ),);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
-
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graduation_project/core/cache/cache_helper.dart';
 import 'package:graduation_project/core/utils/app_router.dart';
-import 'package:graduation_project/core/api/dio_helper.dart';
 import 'core/bloc/bloc_observer.dart';
-import 'features/auth/presentation/view_models/cubit.dart';
+
 
 
 void main() async{
@@ -16,9 +13,7 @@ void main() async{
  await CacheHelper().init();
   Bloc.observer = MyBlocObserver();
    runApp(
-     BlocProvider(
-         create: (context) => AppAuthCubit( DioHelper(dio: Dio()),),
-         child:  const MyApp()),
+     const MyApp()
   );
 }
 
