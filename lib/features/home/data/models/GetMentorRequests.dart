d7dc1262cc293e0c1c97e8b00fd2df43542077d6
Mohammed Ashraf <mohamedasharf55@gmail.com>
2024-05-04 16:59:49 +0300

    Send & Get models requests

---
+++ lib/features/home/data/models/GetMentorRequests.dart
@@ -0,0 +1,66 @@
+class GetMentorRequests {
+
+  String? message;
+  List<Result>? result;
+
+  GetMentorRequests({
+      this.message, 
+      this.result,});
+
+  GetMentorRequests.fromJson(dynamic json) {
+    message = json['message'];
+    if (json['result'] != null) {
+      result = [];
+      json['result'].forEach((v) {
+        result?.add(Result.fromJson(v));
+      });
+    }
+  }
+
+}
+class Mentor {
+  String? id;
+  String? name;
+  String? email;
+
+  Mentor({
+    this.id,
+    this.name,
+    this.email,});
+
+  Mentor.fromJson(dynamic json) {
+    id = json['_id'];
+    name = json['name'];
+    email = json['email'];
+  }
+
+}
+class Result {
+  String? id;
+  String? patient;
+  Mentor? mentor;
+  String? status;
+  String? createdAt;
+  String? updatedAt;
+  num? v;
+
+  Result({
+    this.id,
+    this.patient,
+    this.mentor,
+    this.status,
+    this.createdAt,
+    this.updatedAt,
+    this.v,});
+
+  Result.fromJson(dynamic json) {
+    id = json['_id'];
+    patient = json['patient'];
+    mentor = json['mentor'] != null ? Mentor.fromJson(json['mentor']) : null;
+    status = json['status'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    v = json['__v'];
+  }
+
+}
---
+++ lib/features/home/data/models/SendCareRequest.dart
@@ -0,0 +1,35 @@
+class SendCareRequest {
+  String? message;
+  Result? result;
+
+  SendCareRequest({
+      this.message, 
+      this.result,});
+
+  SendCareRequest.fromJson(dynamic json) {
+    message = json['message'];
+    result = json['result'] != null ? Result.fromJson(json['result']) : null;
+  }
+
+}
+class Result {
+  String? patient;
+  String? mentor;
+  String? status;
+  String? id;
+
+  Result({
+    this.patient,
+    this.mentor,
+    this.status,
+    this.id,});
+
+  Result.fromJson(dynamic json) {
+    patient = json['patient'];
+    mentor = json['mentor'];
+    status = json['status'];
+    id = json['_id'];
+  }
+
+
+}
